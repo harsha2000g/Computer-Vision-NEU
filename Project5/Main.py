a78f55bd0d4e8e1b9fd8ccd7475485584f1b853c
Harsha <harsha2000g@gmail.com>
2023-03-29 19:34:02 -0400

    Project 5

---
+++ Project5/Main.py
@@ -0,0 +1,32 @@
+# Sri Harsha Gollamudi
+# This code is 
+
+# import statements
+import sys
+import torch
+import torch.nn as nn
+import torchvision
+
+# class definitions
+class MyNetwork(nn.Module):
+    def __init__(self):
+        pass
+
+    # computes a forward pass for the network
+    # methods need a summary comment
+    def forward(self, x):
+        return x
+
+# useful functions with a comment for each function
+def train_network( arguments ):
+    return
+
+# main function (yes, it needs a comment too)
+def main(argv):
+    # handle any command line arguments in argv
+
+    # main function code
+    return
+
+if __name__ == "__main__":
+    main(sys.argv)
